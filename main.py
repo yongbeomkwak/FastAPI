c73b89ef23d85fd249941aa777ebb313560aeed1
yongbeomkwak <yongbeomkwak@gmail.com>
2024-09-28 09:55:10 +0900

    DOCS

--- main.py
+++ main.py
@@ -6,68 +6,14 @@ from dto import *
 
 app = FastAPI()
 
-@app.get("/")
-
+@app.get("/") #클라이언트가 HTTP METHOD중 GET을 통해 localhost: 8000에 접속햇을 때 담당할 함수
 async def read_root():
     return {"Hello" : "Wrold" }
 
-# 고정 경로가 항상 위에 
-@app.get("/users/me")
-async def read_user_me():
-    return {"user_id": "the current user"}
-
-# 열거형 활용
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name is ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
-
-    elif model_name.value == "lenet":
-        return {"model_name": model_name, "message": "LeCNN all the images"}
-
-    else:
-        return {"model_name": model_name, "message": "Have some residuals"}
-    
+@app.get("/sat") #클라이언트가 HTTP METHOD중 GET을 통해 localhost: 8000/sat에 접속했을 때 담당하는 함수
+async def read_sat():
+    return {"Hello" : "토요일" }
 
 @app.get("/items/{item_id}")
-async def read_user_item(
-    item_id: str, needy: str, skip: int = 0, limit: Union[int, None] = None
-):
-    
-    #needy, 필수적인 str. = 기본 값 없음
-    #skip, 기본값이 0인 int. = 기본값 있음
-    #limit, 선택적인 int. = 선택적 파라미터, 값이 있으면 int여야하고 없으면 None
-
-    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
-    return item
-
-
-
-@app.put("/items/{item_id}")
-async def update_item(item_id: int, item: ItemDTO, q: str | None = None):
-    print(item_id, item, q)
-    result = {"item_id": item_id}
-    if q:
-        result.update({"q": q})
-    return result
-
-
-## 쿼리 파라미터 다양한 검증
-@app.get("/items/")
-async def read_items(
-    q: Union[str, None] = Query(
-        default=None, # 기본 값 
-        alias="item-query", # 유저가 요청할 때 쓰일 별칭 값 
-        title="임시 타이틀", # 
-        description="Query string for the items to search in the database that have a good match", # 설명
-        min_length=3, # 최소 길이
-        max_length=50, # 최대 길이
-        pattern="^fixedquery$", # 패턴 설정
-        # deprecated=True, # swagger에 deprecated 표시
-    ),
-):
-    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
-    if q:
-        results.update({"q": q})
-    return results
-
+async def update_item(item_id: int):
+    return {"item_id": item_id}
